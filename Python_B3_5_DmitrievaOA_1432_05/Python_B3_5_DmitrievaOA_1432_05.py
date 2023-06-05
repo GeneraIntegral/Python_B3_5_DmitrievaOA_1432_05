923c44ff2eebcee0ce684f7198f25ccf841331ad
User <User@WIN-NCFEC32DCAH>
2023-06-05 14:55:49 +0300

    Добавьте файлы проекта.

---
+++ Python_B3_5_DmitrievaOA_1432_05/Python_B3_5_DmitrievaOA_1432_05.py
@@ -0,0 +1,6 @@
+﻿words = input("Enter a list of words separated by commas: ").split(",") # Введите список слов через запятую:
+word_set = set(words)
+print("Number of words:", len(words)) # Количество слов:
+second_list = input("Enter a list of words with the same number of characters: ").split(",") # Введите список слов такого же количества символов:
+dictionary = dict(zip(words, second_list))
+print(dictionary)
